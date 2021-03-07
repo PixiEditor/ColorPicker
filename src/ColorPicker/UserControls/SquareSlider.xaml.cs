f5595f46d28b362051532f76857bc07ce588dc5d
Equbuxu <egor08281@gmail.com>
2021-03-07 04:51:24 +0300

    Add scalable color display

--- src/ColorPicker/UserControls/SquareSlider.xaml.cs
+++ src/ColorPicker/UserControls/SquareSlider.xaml.cs
@@ -1,19 +1,13 @@
 ﻿using ColorPicker.Models;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ColorPicker
+namespace ColorPicker.UserControls
 {
     internal partial class SquareSlider : UserControl, INotifyPropertyChanged
     {
