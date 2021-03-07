8b40952fe59681e924e5b89b3f120f0811a678ed
Equbuxu <egor08281@gmail.com>
2021-03-07 11:13:02 +0300

    introduce color sliders smallchange dependecy property

--- src/ColorPicker/ColorSliders.xaml.cs
+++ src/ColorPicker/ColorSliders.xaml.cs
@@ -1,7 +1,17 @@
-﻿namespace ColorPicker
+﻿using System.Windows;
+
+namespace ColorPicker
 {
     public partial class ColorSliders : PickerControlBase
     {
+        public static readonly DependencyProperty SmallChangeProperty =
+            DependencyProperty.Register(nameof(SmallChange), typeof(double), typeof(ColorSliders),
+                new PropertyMetadata(1.0));
+        public double SmallChange
+        {
+            get => (double)GetValue(SmallChangeProperty);
+            set => SetValue(SmallChangeProperty, value);
+        }
         public ColorSliders() : base()
         {
             InitializeComponent();
