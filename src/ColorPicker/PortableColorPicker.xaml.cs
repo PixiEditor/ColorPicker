90b5e1ed22a443ef38616f7eac226e5b45a12481
Equbuxu <egor08281@gmail.com>
2021-03-07 13:55:27 +0300

    Remake PortableColorPicker

--- src/ColorPicker/PortableColorPicker.xaml.cs
+++ src/ColorPicker/PortableColorPicker.xaml.cs
@@ -1,26 +1,12 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace ColorPicker
 {
-    public partial class PortableColorPicker : UserControl
+    public partial class PortableColorPicker : DualPickerControlBase
     {
-        public static readonly DependencyProperty SelectedColorProperty =
-            DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(PortableColorPicker),
-                new PropertyMetadata(Colors.Black));
-
-        public static readonly DependencyProperty SecondaryColorProperty =
-            DependencyProperty.Register(nameof(SecondaryColor), typeof(Color), typeof(PortableColorPicker),
-                new PropertyMetadata(Colors.White));
-
-        public static readonly DependencyProperty HueSmallChangeProperty =
-            DependencyProperty.Register(nameof(HueSmallChange), typeof(double), typeof(PortableColorPicker),
-                new PropertyMetadata(1.0));
-
         public static readonly DependencyProperty SmallChangeProperty =
             DependencyProperty.Register(nameof(SmallChange), typeof(double), typeof(PortableColorPicker),
-                new PropertyMetadata(0.00390625));
+                new PropertyMetadata(1.0));
 
         public double SmallChange
         {
@@ -28,29 +14,9 @@ namespace ColorPicker
             set => SetValue(SmallChangeProperty, value);
         }
 
-        public double HueSmallChange
-        {
-            get => (double)GetValue(HueSmallChangeProperty);
-            set => SetValue(HueSmallChangeProperty, value);
-        }
-
         public PortableColorPicker()
         {
             InitializeComponent();
-        }
-
-
-        public Color SelectedColor
-        {
-            get => (Color) GetValue(SelectedColorProperty);
-            set => SetValue(SelectedColorProperty, value);
-        }
-
-
-        public Color SecondaryColor
-        {
-            get => (Color) GetValue(SecondaryColorProperty);
-            set => SetValue(SecondaryColorProperty, value);
         }
     }
 }
