15f79592c97113fe14b1239153aea7eaee05b867
Equbuxu <egor08281@gmail.com>
2021-03-17 01:46:00 +0300

    Implement ShowAlpha for Standard/PortableColorPicker

--- src/ColorPicker/PortableColorPicker.xaml.cs
+++ src/ColorPicker/PortableColorPicker.xaml.cs
@@ -8,10 +8,20 @@ namespace ColorPicker
             DependencyProperty.Register(nameof(SmallChange), typeof(double), typeof(PortableColorPicker),
                 new PropertyMetadata(1.0));
 
+        public static readonly DependencyProperty ShowAlphaProperty =
+            DependencyProperty.Register(nameof(ShowAlpha), typeof(bool), typeof(PortableColorPicker),
+                new PropertyMetadata(true));
+
         public double SmallChange
         {
             get => (double)GetValue(SmallChangeProperty);
             set => SetValue(SmallChangeProperty, value);
+        }
+
+        public bool ShowAlpha
+        {
+            get => (bool)GetValue(ShowAlphaProperty);
+            set => SetValue(ShowAlphaProperty, value);
         }
 
         public PortableColorPicker()
--- src/ColorPicker/StandardColorPicker.xaml.cs
+++ src/ColorPicker/StandardColorPicker.xaml.cs
@@ -8,10 +8,20 @@ namespace ColorPicker
             DependencyProperty.Register(nameof(SmallChange), typeof(double), typeof(StandardColorPicker),
                 new PropertyMetadata(1.0));
 
+        public static readonly DependencyProperty ShowAlphaProperty =
+            DependencyProperty.Register(nameof(ShowAlpha), typeof(bool), typeof(StandardColorPicker),
+                new PropertyMetadata(true));
+
         public double SmallChange
         {
             get => (double)GetValue(SmallChangeProperty);
             set => SetValue(SmallChangeProperty, value);
+        }
+
+        public bool ShowAlpha
+        {
+            get => (bool)GetValue(ShowAlphaProperty);
+            set => SetValue(ShowAlphaProperty, value);
         }
 
         public StandardColorPicker() : base()
