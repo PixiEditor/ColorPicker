312b7bd1595b09014201ba707385e6b426ba4ccf
Equbuxu <egor08281@gmail.com>
2021-03-17 03:30:37 +0300

    fix hex field styling

--- src/ColorPicker/HexColorTextBox.xaml.cs
+++ src/ColorPicker/HexColorTextBox.xaml.cs
@@ -1,18 +1,30 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
 
 namespace ColorPicker
 {
-    public partial class HexColorTextBox : PickerControlBase
+    public partial class HexColorTextBox : PickerControlBase, INotifyPropertyChanged
     {
         public static readonly DependencyProperty ShowAlphaProperty =
             DependencyProperty.Register(nameof(ShowAlpha), typeof(bool), typeof(HexColorTextBox),
                 new PropertyMetadata(true));
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public bool ShowAlpha
         {
             get => (bool)GetValue(ShowAlphaProperty);
-            set => SetValue(ShowAlphaProperty, value);
+            set
+            {
+                SetValue(ShowAlphaProperty, value);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextBoxConverter)));
+            }
+        }
+
+        public IValueConverter TextBoxConverter
+        {
+            get => ShowAlpha ? (IValueConverter)Resources["ColorToLongHexConverter"] : (IValueConverter)Resources["ColorToShortHexConverter"];
         }
 
         public HexColorTextBox() : base()
