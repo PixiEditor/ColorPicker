9b76f5fb6e90e791a5e417635c2c25f7096a09b5
Equbuxu <egor08281@gmail.com>
2021-03-30 21:11:15 +0300

    Eliminate System.Windows.Interactivity (Resolve #8)

--- src/ColorPicker/Behaviors/TextBoxFocusBehavior.cs
+++ src/ColorPicker/Behaviors/TextBoxFocusBehavior.cs
@@ -1,7 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Interactivity;
 
 namespace ColorPicker.Behaviors
 {
