97bdde6ac5a86e99c0559899ab988b7b16a591cf
Equbuxu <egor08281@gmail.com>
2023-08-27 00:14:30 +0300

    Rename UseNumericValues to ShowFractionalPart, showcase it in the demo

--- src/ColorPicker/Converters/TextBoxTextStringFormatMultiValueConverter.cs
+++ src/ColorPicker/Converters/TextBoxTextStringFormatMultiValueConverter.cs
@@ -4,28 +4,28 @@ using System.Windows.Data;
 
 namespace ColorPicker.Converters
 {
-  public class TextBoxTextStringFormatMultiValueConverter : IMultiValueConverter
-  {
-    public string NumericFormat { get; set; } = "N1";
-    
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    public class TextBoxTextStringFormatMultiValueConverter : IMultiValueConverter
     {
-      if (values.Length != 2 || !(values[0] is double doubleVal) || !(values[1] is bool useNumericValues))
-      {
-        throw new ArgumentException("Values array should contain 2 elements", nameof(values));
-      }
+        private readonly string numericFormat = "N1";
 
-      return doubleVal.ToString(useNumericValues ? NumericFormat : "N0");
-    }
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2 || !(values[0] is double doubleVal) || !(values[1] is bool showFractionalPart))
+            {
+                throw new ArgumentException("Values array should contain 2 elements", nameof(values));
+            }
 
-    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-    {
-      if (!double.TryParse((value.ToString()).Replace(',', '.'), NumberStyles.Float, CultureInfo.InvariantCulture, out double result))
-      {
-        return new[] { Binding.DoNothing, Binding.DoNothing };
-      }
-      
-      return new[] { result.ToString(NumericFormat), Binding.DoNothing };
+            return doubleVal.ToString(showFractionalPart ? numericFormat : "N0");
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            if (!double.TryParse((value.ToString()).Replace(',', '.'), NumberStyles.Float, CultureInfo.InvariantCulture, out double result))
+            {
+                return new[] { Binding.DoNothing, Binding.DoNothing };
+            }
+
+            return new[] { result.ToString(numericFormat), Binding.DoNothing };
+        }
     }
-  }
 }
