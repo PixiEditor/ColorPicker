038c294e6bcf35161024b059f32d49dcfd4de23e
Krzysztof Krysiński <flubbet@gmail.com>
2023-11-03 11:04:22 +0100

    Fixed hex binding

---
+++ src/ColorPicker.AvaloniaUI/Behaviors/HexTextBoxBindingBehavior.cs
@@ -0,0 +1,39 @@
+using Avalonia;
+using Avalonia.Media;
+using ColorPicker.Converters;
+using ColorPicker.Models;
+
+namespace ColorPicker.Behaviors;
+
+internal class HexTextBoxBindingBehavior : LostFocusUpdateBindingBehavior
+{
+    public static readonly StyledProperty<Color> ColorProperty = AvaloniaProperty.Register<HexTextBoxBindingBehavior, Color>(
+        "Color");
+
+    public Color Color
+    {
+        get => GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+
+    public static readonly StyledProperty<ColorToHexConverter> HexConverterProperty = AvaloniaProperty.Register<HexTextBoxBindingBehavior, ColorToHexConverter>(
+        "HexConverter");
+
+    public ColorToHexConverter HexConverter
+    {
+        get => GetValue(HexConverterProperty);
+        set => SetValue(HexConverterProperty, value);
+    }
+
+    protected override void OnSubmitValue(string oldValue, string newValue)
+    {
+        object value = HexConverter.ConvertBack(newValue, typeof(string), null, null);
+        if (value is not Avalonia.Media.Color color)
+        {
+            Text = oldValue;
+            return;
+        }
+
+        Color = color;
+    }
+}
---
+++ src/ColorPicker.AvaloniaUI/Behaviors/LostFocusUpdateBindingBehavior.cs
@@ -0,0 +1,64 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Interactivity;
+using Avalonia.Reactive;
+using Avalonia.Xaml.Interactivity;
+
+namespace ColorPicker.Behaviors;
+
+public class LostFocusUpdateBindingBehavior : Behavior<TextBox>
+{
+    static LostFocusUpdateBindingBehavior()
+    {
+        TextProperty.Changed.Subscribe(new AnonymousObserver<AvaloniaPropertyChangedEventArgs<string>>(e =>
+        {
+            ((LostFocusUpdateBindingBehavior) e.Sender).OnBindingValueChanged();
+        }));
+    }
+
+
+    public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<LostFocusUpdateBindingBehavior, string>(
+        "Text", defaultBindingMode: BindingMode.TwoWay);
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        AssociatedObject.LostFocus += OnLostFocus;
+        base.OnAttached();
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.LostFocus -= OnLostFocus;
+        base.OnDetaching();
+    }
+
+    private void OnLostFocus(object? sender, RoutedEventArgs e)
+    {
+        if (AssociatedObject != null)
+        {
+            string oldValue = Text;
+            Text = AssociatedObject.Text;
+            OnSubmitValue(oldValue, Text);
+        }
+    }
+
+    private void OnBindingValueChanged()
+    {
+        if (AssociatedObject != null)
+        {
+            AssociatedObject.Text = Text;
+        }
+    }
+
+    protected virtual void OnSubmitValue(string oldValue, string newValue)
+    {
+
+    }
+}
