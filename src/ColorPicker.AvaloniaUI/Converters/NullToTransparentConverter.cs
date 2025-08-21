214215203d18b4088730311c2023aeddae731516
Krzysztof Krysiński <flubbet@gmail.com>
2025-08-21 15:47:22 +0200

    Fixed null brush in portable color picker

---
+++ src/ColorPicker.AvaloniaUI/Converters/NullToTransparentConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ColorPicker.Converters;
+
+public class NullToTransparentConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return Avalonia.Media.Brushes.Transparent;
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
